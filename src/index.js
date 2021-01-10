b380f82e097514526fde833626a283750c71b2cc
mbenitezm <marcelbm94@gmail.com>
2021-01-10 00:14:21 -0600

    remove pupeteer, add new relic

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+import newrelic from 'newrelic';
 import discord from 'discord.js';
-import logger from 'morgan';
 import calculate from './lib/calculator';
 import share from './lib/share';
-import yasir from './lib/yasir';
+//import yasir from './lib/yasir';
 import rashid from './lib/rashid';
 import servers from './lib/servers';
 
@@ -12,72 +12,76 @@ discordClient.on('ready', () => {
   console.log(`Logged in as ${discordClient.user.tag}!`);
 });
 
-discordClient.on('message', async (msg) => {
-  if (msg.member.user.id === process.env.BOT_ID) {
-    return;
-  }
-
-  if (msg.content.includes('!session')) {
-    try {
-      let response = calculate(msg.content);
-      console.log(response);
-      msg.reply(response);
-    } catch (error) {
-      msg.reply("I couldn't understand your request");
+newrelic.startWebTransaction('onMessage', () => {
+  let transaction = newrelic.getTransaction();
+  discordClient.on('message', async (msg) => {
+    if (msg.member.user.id === process.env.BOT_ID) {
+      return;
     }
-  }
 
-  if (msg.content.includes('!share')) {
-    try {
-      const response = await share(msg.content);
-      console.log(response);
-      msg.reply(response);
-    } catch (error) {
-      msg.reply("I couldn't understand your request");
+    if (msg.content.includes('!session')) {
+      try {
+        let response = calculate(msg.content);
+        console.log(response);
+        msg.reply(response);
+      } catch (error) {
+        msg.reply("I couldn't understand your request");
+      }
     }
-  }
 
-  if (msg.content.includes('!admincommand-servers')) {
-    try {
-      msg.reply(servers(discordClient));
-    } catch (error) {
-      console.log(error);
-      msg.reply("I couldn't understand your request");
+    if (msg.content.includes('!share')) {
+      try {
+        const response = await share(msg.content);
+        console.log(response);
+        msg.reply(response);
+      } catch (error) {
+        msg.reply("I couldn't understand your request");
+      }
     }
-  }
 
-  if (msg.content.includes('!isyasironline')) {
-    msg.reply(
-      "I'm sorry, this is currently not working :(. I'll try to find yasir again soon!"
-    );
-    // try {
-    //   msg.reply('Please wait a few seconds while I search for Yasir.');
-    //   const response = await yasir(msg.content);
-    //   console.log(response);
-    //   msg.reply(response);
-    // } catch (error) {
-    //   console.log(error);
-    //   msg.reply(
-    //     "There was an error checking if yasir was online, I'm sorry :("
-    //   );
-    // }
-  }
+    if (msg.content.includes('!admincommand-servers')) {
+      try {
+        msg.reply(servers(discordClient));
+      } catch (error) {
+        console.log(error);
+        msg.reply("I couldn't understand your request");
+      }
+    }
 
-  if (msg.content.includes('!rashid')) {
-    const location = rashid();
-    msg.reply(location.city, {
-      files: [location.image],
-    });
-  }
+    if (msg.content.includes('!isyasironline')) {
+      msg.reply(
+        "I'm sorry, this is currently not working :(. I'll try to find yasir again soon!"
+      );
+      // try {
+      //   msg.reply('Please wait a few seconds while I search for Yasir.');
+      //   const response = await yasir(msg.content);
+      //   console.log(response);
+      //   msg.reply(response);
+      // } catch (error) {
+      //   console.log(error);
+      //   msg.reply(
+      //     "There was an error checking if yasir was online, I'm sorry :("
+      //   );
+      // }
+    }
 
-  if (msg.content.includes('!help')) {
-    const reply = `Available commands: \n
-             !share <level> \n
-             !session <data copied to clipboard from party hunt analyzer> \n
-             !rashid \n
-             *Made by Nevaux on Calmera :D`;
-    msg.reply(reply);
-  }
+    if (msg.content.includes('!rashid')) {
+      const location = rashid();
+      msg.reply(location.city, {
+        files: [location.image],
+      });
+    }
+
+    if (msg.content.includes('!help')) {
+      const reply = `Available commands: \n
+               !share <level> \n
+               !session <data copied to clipboard from party hunt analyzer> \n
+               !rashid \n
+               *Made by Nevaux on Calmera :D`;
+      msg.reply(reply);
+    }
+  });
+  transaction.end();
 });
 
 discordClient.login(process.env.DISCORD_SECRET);
--- src/lib/yasir.js
+++ src/lib/yasir.js
@@ -1,32 +1,36 @@
-import puppeteer from 'puppeteer';
-import cheerio from 'cheerio';
-import fs from 'fs';
-import { parseYasirWorlds } from './parser';
+// import puppeteer from 'puppeteer';
+// import cheerio from 'cheerio';
+// import fs from 'fs';
+// import { parseYasirWorlds } from './parser';
 
-const isYasirOnline = async () => {
-  const html = await fetchData(process.env.YASIR_SITE);
-  let $ = cheerio.load(html);
+// const isYasirOnline = async () => {
+//   const html = await fetchData(process.env.YASIR_SITE);
+//   let $ = cheerio.load(html);
 
-  const gameWorlds = await $('.online')
-    .contents()
-    .map((i, e) => $(e).text())
-    .toArray();
+//   const gameWorlds = await $('.online')
+//     .contents()
+//     .map((i, e) => $(e).text())
+//     .toArray();
 
-  const result = parseYasirWorlds(gameWorlds);
-  return formatResult(result);
-};
+//   const result = parseYasirWorlds(gameWorlds);
+//   return formatResult(result);
+// };
 
-const fetchData = async (url) => {
-  const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
-  const page = await browser.newPage();
-  await page.goto(url);
-  await page.waitFor(1500);
-  const html = await page.content();
-  return html;
-};
+// const fetchData = async (url) => {
+//   const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+//   const page = await browser.newPage();
+//   await page.goto(url);
+//   await page.waitFor(1500);
+//   const html = await page.content();
+//   return html;
+// };
 
-const formatResult = (result) => {
-  return 'Yasir is online in: \n' + result.join('\n');
-};
+// const formatResult = (result) => {
+//   return 'Yasir is online in: \n' + result.join('\n');
+// };
 
-export default isYasirOnline;
+// export default isYasirOnline;
+
+// // when enable again
+// // https://github.com/jontewks/puppeteer-heroku-buildpack
+// // "puppeteer": "^5.3.1"
